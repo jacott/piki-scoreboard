4b363e6be62cae5281617d79744dc6e5b071171a
Geoff Jacobsen <geoffjacobsen@gmail.com>
2014-02-14 12:29:57 +1300

    Show ChooseOrg page if no org chosen

--- app/client/js/home.js
+++ app/client/js/home.js
@@ -1,5 +1,25 @@
+var $ = Bart.current;
 var Tpl = Bart.Home;
+var ChooseOrg = Tpl.ChooseOrg;
 
-AppRoute.root.addTemplate(Tpl, {path: ""});
+AppRoute.root.addTemplate(Tpl, {
+  path: "",
+  data: function (page, pageRoute) {
+    if (! ('orgSN' in pageRoute)) AppRoute.abortPage(ChooseOrg);
+  }
+});
 
+AppRoute.root.addTemplate(ChooseOrg);
 AppRoute.root.defaultPage = Tpl;
+
+
+ChooseOrg.$helpers({
+  orgs: function (callback) {
+    AppModel.Org.find({}, {sort: {name: 1}})
+      .forEach(function (doc) {callback(doc)});
+
+    $.ctx.onDestroy(AppModel.Org.Index.observe(function (doc, old) {
+      callback(doc, old, Apputil.compareByName);
+    }));
+  },
+});
--- test/client/home-test.js
+++ test/client/home-test.js
@@ -9,39 +9,57 @@
       v = null;
     },
 
-    "test rendering": function () {
-      assert.same(AppRoute.root.defaultPage, Bart.Home);
-
+    "test Choose org": function() {
+      v.org = TH.Factory.createOrg();
       AppRoute.gotoPage(Bart.Home);
+
       test.stub(AppRoute, 'gotoPath');
-
-      assert.dom('#Home', function () {
-        TH.click('button.link', 'Clubs');
-        assert.calledWith(AppRoute.gotoPath, Bart.Club);
-
-        TH.click('button.link', 'Climbers');
-        assert.calledWith(AppRoute.gotoPath, Bart.Climber);
-
-        TH.click('button.link', 'Events');
-        assert.calledWith(AppRoute.gotoPath, Bart.Event);
-
-        TH.click('button.link', 'Event categories');
-        assert.calledWith(AppRoute.gotoPath, Bart.Category);
+      assert.dom('#ChooseOrg', function () {
+        TH.click('.link', v.org.name);
       });
+
+      assert.calledWith(AppRoute.gotoPath, v.org.shortName);
     },
 
-    "test climbers link": function () {
-      AppRoute.gotoPage(Bart.Home);
-      TH.click('button.link', 'Climbers');
+    "with org": {
+      setUp: function () {
+        TH.setOrg();
+      },
 
-      assert.dom('#Climber');
-    },
+      "test rendering": function () {
+        assert.same(AppRoute.root.defaultPage, Bart.Home);
 
-    "test events link": function () {
-      AppRoute.gotoPage(Bart.Home);
-      TH.click('button.link', 'Events');
+        AppRoute.gotoPage(Bart.Home);
+        test.stub(AppRoute, 'gotoPath');
 
-      assert.dom('#Event');
+        assert.dom('#Home', function () {
+          TH.click('button.link', 'Clubs');
+          assert.calledWith(AppRoute.gotoPath, Bart.Club);
+
+          TH.click('button.link', 'Climbers');
+          assert.calledWith(AppRoute.gotoPath, Bart.Climber);
+
+          TH.click('button.link', 'Events');
+          assert.calledWith(AppRoute.gotoPath, Bart.Event);
+
+          TH.click('button.link', 'Event categories');
+          assert.calledWith(AppRoute.gotoPath, Bart.Category);
+        });
+      },
+
+      "test climbers link": function () {
+        AppRoute.gotoPage(Bart.Home);
+        TH.click('button.link', 'Climbers');
+
+        assert.dom('#Climber');
+      },
+
+      "test events link": function () {
+        AppRoute.gotoPage(Bart.Home);
+        TH.click('button.link', 'Events');
+
+        assert.dom('#Event');
+      },
     },
   });
 })();
