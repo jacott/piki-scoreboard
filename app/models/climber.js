7cbdd563686c4897f4c0b1233f46bd04fd27e161
Geoff Jacobsen <geoffjacobsen@gmail.com>
2014-02-27 23:51:09 +1300

    Fix Climber club_id validation

--- app/models/climber.js
+++ app/models/climber.js
@@ -6,7 +6,7 @@ App.require('AppModel.Club', function () {
   model.defineFields({
     name: {type:  'text', trim: true, required: true, maxLength: 200},
     org_id: 'belongs_to',
-    club_id: 'belongs_to',
+    club_id: {type: 'belongs_to', required: true},
     dateOfBirth: {type: 'date', inclusion: {matches: /^\d{4}-[01]\d-[0-3]\d$/}},
     gender: {type: 'text', required: true, inclusion: {allowBlank: true, matches: /^[mf]$/ }},
   });
