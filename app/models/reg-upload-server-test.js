fe661b58dc8dd9521035164ee81be162b2827c14
Geoff Jacobsen <geoffjacobsen@gmail.com>
2014-06-19 17:48:50 +1200

    Port server-side reg-upload

---
+++ app/models/reg-upload-server-test.js
@@ -0,0 +1,104 @@
+define(function (require, exports, module) {
+  var test, v;
+  var koru = require('koru');
+  var TH = require('test-helper');
+  require('./reg-upload-server');
+  var session = require('koru/session');
+  var Club = require('models/club');
+  var Climber = require('models/climber');
+  var Competitor = require('./competitor');
+  var User = require('models/user');
+  var Event = require('models/event');
+
+  TH.testCase(module, {
+    setUp: function () {
+      test = this;
+      v = {};
+      v.org =TH.Factory.createOrg();
+      v.user = TH.Factory.createUser({role: User.ROLE.admin});
+      v.event = TH.Factory.createEvent({heats: undefined});
+      v.club = TH.Factory.createClub({name: 'Rock Hoppers'});
+      v.fjl = TH.Factory.createCategory({shortName: 'FJL', gender: 'f', name: 'Female Junior Lead', group: 'Youth Lead'});
+      v.mjl = TH.Factory.createCategory({shortName: 'MJL', gender: 'm', name: 'Male Junior Lead', group: 'Youth Lead'});
+      v.fol = TH.Factory.createCategory({shortName: 'FOL', gender: 'f', name: 'Female Open Lead', group: 'Open Lead'});
+      v.sam = TH.Factory.createClimber({name: 'Sam Smith', dateOfBirth: '1996-04-16'});
+      TH.loginAs(v.user);
+
+      v.rpc = TH.mockRpc();
+    },
+
+    tearDown: function () {
+      TH.clearDB();
+      v = null;
+    },
+
+    'test access denied': function () {
+      test.stub(koru, 'info');
+      assert.exception(function () {
+        v.rpc('Reg.upload', '123', "this ain't valid csv;");
+      }, {error: 403, reason: 'Access denied'});
+    },
+
+    'test invalid file': function () {
+      test.stub(koru, 'info');
+      assert.exception(function () {
+        v.rpc('Reg.upload', v.event._id, "this ain't valid csv;");
+      }, {error: 415, reason: 'unsupported_import_format'});
+    },
+
+    "test uploading": function () {
+      var csv =
+            '"Fee level","First Name","Last Name","Birth Date","Participant ID"\n' +
+            '"Rock Hoppers,Junior (Early Bird) [FJL,FOL]","Anna","Smith","1996-04-16","149"\n' +
+            '"Rock Hoppers,Junior (Early Bird) [MJL]","Sam","Smith","1996-04-16","148"\n' +
+            '"Mountain Goats,Junior (Early Bird) [MJL]","Mark","Ford","1995-11-26","230"\n' +
+            '"Rock Hoppers,Junior (Early Bird)","Henry","Smith","2002-04-16","147"\n';
+
+      v.rpc('Reg.upload', v.event._id, csv);
+
+      assert.equals(v.event.$reload().attributes.errors, [[
+        3, {
+          'Fee level': 'Mountain Goats,Junior (Early Bird) [MJL]',
+          'First Name': 'Mark',
+          'Last Name': 'Ford',
+          'Birth Date': '1995-11-26',
+          'Participant ID': '230'}, "Can't find club 'Mountain Goats'"
+      ],[
+        4, {
+          'Fee level': "Rock Hoppers,Junior (Early Bird)",
+          'First Name': 'Henry',
+          'Last Name': 'Smith',
+          'Birth Date': '2002-04-16',
+          'Participant ID': '147'}, "Invalid or missing codes",
+      ]]);
+
+      var club = Club.findByField('name', 'Rock Hoppers');
+      assert(club);
+
+      var climber = Climber.findByField('name', 'Anna Smith');
+      assert(climber);
+
+      assert.same(climber.dateOfBirth, '1996-04-16');
+      assert.same(climber.org_id, v.org._id);
+      assert.same(climber.club_id, v.club._id);
+      assert.same(climber.uploadId, '149');
+      assert.same(climber.gender, 'f');
+
+
+      var competitor = Competitor.findByField('climber_id', climber._id);
+      assert(competitor);
+
+      assert.same(competitor.event_id, v.event._id);
+      assert.equals(competitor.category_ids, [v.fjl._id, v.fol._id].sort());
+
+      assert.equals(Object.keys(v.event.heats).sort(), [v.fjl._id, v.mjl._id, v.fol._id].sort());
+
+      assert.same(Competitor.query.count(), 2);
+
+      // check idempotencey
+      v.rpc('Reg.upload', v.event._id, csv);
+
+      assert.same(Competitor.query.count(), 2);
+    },
+  });
+});
---
+++ app/models/reg-upload-server.js
@@ -0,0 +1,139 @@
+var Future = requirejs.nodeRequire('fibers/future');
+var CSV = require('csv');
+
+define(function(require, exports, module) {
+  var util = require('koru/util');
+  var session = require('koru/session');
+  var Val = require('koru/model/validation');
+  var User = require('models/user');
+  var Event = require('models/event');
+  var koru = require('koru');
+  var Club = require('models/club');
+  var Climber = require('models/climber');
+  var Category = require('models/category');
+  var Competitor = require('models/competitor');
+  require('models/result');
+
+  session.defineRpc('Reg.upload', function (eventId, data) {
+    Val.ensureString(eventId);
+
+    var ROLE = User.ROLE;
+
+    var user = User.query.where({
+      _id: this.userId,
+      role: {$in: [ROLE.superUser, ROLE.admin]}}).fetchOne();
+
+    Val.allowAccessIf(user);
+
+    var event = Event.findById(eventId);
+
+    Val.allowAccessIf(event);
+
+    user.isSuperUser() || Val.allowAccessIf(event.org_id === user.org_id);
+
+    var future = new Future();
+
+    CSV()
+      .from.string(
+        new Buffer(data), {columns: true})
+      .to.array( function(rows){
+        data = rows;
+        future.return();
+      }, {columns: ['Fee level', 'First Name', 'Last Name', 'Birth Date', 'Participant ID']});
+
+    future.wait();
+
+    var errors = [];
+    var row;
+    var climbers = {};
+
+    if (data.length === 1)
+      throw new koru.Error(415, 'unsupported_import_format');
+
+    for(var i = 0; i < data.length; ++i) {
+      row =  data[i];
+      var name = get('First Name') + ' ' + get('Last Name');
+      if (name in climbers)
+        ++climbers[name];
+      else
+        climbers[name] = 1;
+    }
+
+    for(var i = 0; i < data.length; ++i) {
+      try {
+        row =  data[i];
+        importCompetitor();
+      } catch(ex) {
+        errors.push([i+1, data[i], ex.toString()]);
+        }
+    };
+
+    Event.query.onId(event._id).update({errors: errors});
+
+    function importCompetitor() {
+      var name = get('First Name') + ' ' + get('Last Name');
+
+      if (climbers[name] > 1)
+        throw 'Name: "' + name + '" registered ' + climbers[name] + ' times';
+
+      var meta = get('Fee level');
+
+      var clubName = meta.split(',')[0].trim();
+
+      var club = Club.query.where({name: clubName, org_id: event.org_id}).fetchOne();
+      if (! club) throw "Can't find club '" + clubName + "'";
+
+      var codes = /\[(.+)\]/.exec(meta);
+
+      codes = codes && codes[1].trim().split(',');
+
+      var gender = (codes && codes[0][0]) || null;
+      gender = gender && gender.toLowerCase();
+
+      var climber = Climber.query.where({name: name, org_id: event.org_id}).fetchOne();
+
+      if (! climber) {
+        climber = Climber.build({
+          name: name, org_id: event.org_id, club_id: club._id,
+          dateOfBirth: get('Birth Date').trim(),
+          gender: gender,
+          uploadId: get('Participant ID'),
+        });
+      }
+
+      if (climber.dateOfBirth !== get('Birth Date').trim())
+        throw "Climber's date-of-birth does not match: " + climber.dateOfBirth;
+
+      if (! climber.$isValid())
+        throw "Climber: " + Val.inspectErrors(climber);
+
+      climber.$save();
+
+      if (! (codes && codes.length))
+        throw "Invalid or missing codes";
+
+      var category_ids = [];
+
+      codes.forEach(function (code) {
+        code = code.trim();
+        var cat = Category.findByField('shortName', code);
+        if (! cat) {
+          throw "Category not found: " + code;
+        }
+        category_ids.push(cat._id);
+      });
+
+
+      var competitor = Competitor.query.where({event_id: event._id, climber_id: climber._id}).fetchOne() ||
+            Competitor.build({event_id: event._id, climber_id: climber._id});
+
+      competitor.category_ids = category_ids.sort();
+      competitor.$$save();
+    }
+
+    function get(field) {
+      var val = row[field];
+      return val && val.trim();
+    }
+  });
+});
