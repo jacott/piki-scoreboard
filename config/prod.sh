abc6750ea0cf74b63de10cadfebd7a15f41a49dc
Geoff Jacobsen <geoffjacobsen@gmail.com>
2017-04-03 10:27:37 +1200

    Use private address to install prod

--- config/prod.sh
+++ config/prod.sh
@@ -1,5 +1,5 @@
 export KORU_PORT=3500
 export KORU_HOSTNAME=piki.vimaly.com
 
-export KORU_DEST_SERVER=app@$KORU_HOSTNAME
+export KORU_DEST_SERVER=app@piki-prod
 export APP_MAILURL="smtp://app:$(cat /u/private/intra-authtoken.txt)@mail.${APP_MAILDOMAIN}:465"
