9a3f6ff0146a1cc2980801b6549ce5d541c36fb8
Geoff Jacobsen <geoffjacobsen@gmail.com>
2018-03-05 08:54:00 +1300

    fixup bootstrap for role

--- app/server/bootstrap.js
+++ app/server/bootstrap.js
@@ -7,16 +7,16 @@ define(function(require, exports, module) {
   const Random          = require('koru/random');
   const UserAccount     = require('koru/user-account');
   const Org             = require('models/org');
+  const Role            = require('models/role');
   const User            = require('models/user');
 
   function initNewInstall() {
     if (User.query.count(1) === 0) {
       const id = Random.id();
-      User.docs.insert({_id: id, name: "Super User", initials: "SU", email: "su@example.com", role: 's'});
+      User.docs.insert({_id: id, name: "Super User", initials: "SU", email: "su@example.com"});
       UserAccount.createUserLogin({email: "su@example.com", password: 'changeme', userId: id});
-    }
+      Role.docs.insert({_id: id, user_id: id, org_id: null, role: "s"});
 
-    if (Org.query.count(1) === 0) {
       Org.create({name: 'Example org', shortName: 'EG', email: "su@example.com"});
     }
   }
