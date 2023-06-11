b4983ddbc2aa2e775d47b16d603dafee865a9eb6
Geoff Jacobsen <geoffjacobsen@gmail.com>
2023-06-12 10:52:07 +1200

    feat: add kafe config files

--- config/local.sh
+++ config/local.sh
@@ -1,4 +1,6 @@
 export KORU_PORT=3500
-export APP_DOMAIN=piki.vimaly.lxd
+export VIMALY_DOMAIN=vimaly.lxd
+export APP_DOMAIN=piki.$VIMALY_DOMAIN
+export KORU_DB=piki
 
 export KORU_DEST_SERVER=app@$APP_DOMAIN
--- config/prod.sh
+++ config/prod.sh
@@ -3,4 +3,4 @@ export VIMALY_DOMAIN=trainingsims.com
 export APP_DOMAIN=piki.$VIMALY_DOMAIN
 export KORU_DB=piki
 
-export KORU_DEST_SERVER=app@piki.trainingsims.com
+export KORU_DEST_SERVER=app@$APP_DOMAIN
